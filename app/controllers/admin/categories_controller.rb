b7ad0384d55f40f81b8b30da231923aaf78f4047
mmkarim <md.muzahidul.karim@framgia.com>
2015-09-16 16:59:00 +0700

    Added Lesson Create and Update Admin Module

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,11 +1,40 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :admin_user, only: [:new, :edit, :update, :create]
+
   def new
     @category = Category.new
   end
 
   def edit
+    @category = Category.find params[:id]
+  end
+
+  def update
+    @category = Category.find params[:id]
+    if @category.update_attributes(category_params)
+      flash[:success] = t :lesson_updated
+      redirect_to lessons_path
+    else
+      render :edit
+    end
   end
 
   def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t :lesson_created
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title, :info)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915085526) do
+ActiveRecord::Schema.define(version: 20150916103450) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -46,10 +46,11 @@ ActiveRecord::Schema.define(version: 20150915085526) do
     t.string   "name"
     t.string   "email"
     t.string   "avatar"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
